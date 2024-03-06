b2b33a3ae083423054b8f37558accb09a012c24d
AsadbekNurmamatov2002 <nurmamatovasadbek85@gmail.com>
2024-03-06 12:44:03 -0600

    m2

---
+++ py02.py
@@ -0,0 +1,14 @@
+a=input("baxoinggizni kiriting\ta=")
+
+if int(a)==5:
+    print("alo")
+elif int(a)<5 and int(a)>=4:
+    print("yaxshi")
+elif int(a)<4 and a>=3:
+    print("qoniqarli")
+elif int(a)>=2 and int(a)<3:
+    print("yomon")
+else:
+    print("bunday baxo yo\'q!!)
+
+
