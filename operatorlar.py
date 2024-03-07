e14242093bb34197c2fdc18fee905c119969c8af
Asadbek <144318530+AsadbekNurmamatov2002@users.noreply.github.com>
2024-03-07 15:36:48 +0500

    Create operatorlar.py

---
+++ operatorlar.py
@@ -0,0 +1,63 @@
+# python operatorlari
+"""
+Arfmatetik operatorlar
+O'zlashtirish operatorlar
+Toqqoslash operatorlar
+Mantiqiy operatorlar
+Aniqlash opertorlar
+A'zolik operatorlar
+Bitli operatorlar
+"""
+
+# Arfmetik operatorlar
+"""
++ Qo'shish x+y
+- Ayirish x-y
+* Ko'paytieish x*y
+/ bo'lish x/y
+% qoldiqli bo'lish x%y
+// butunli bo'lish x//y
+"""
+# O'zlashtirish operatorlar
+"""
+=   x=5  x=5
++=  x+=5   x=x+5
+-=   x-=5  x=x-5
+*=  x*=5  x=x*5
+/=  x/=5  x=x/5
+%=  x%=5  x=x%5
+//=  x//=5  x=x//5
+**=  x**=5  x=x**5
+"""
+# Toqqoslash operatorlar
+"""
+== Teng x==y
+!= Teng emas x!=y
+>  Ketta x>y
+< Kichik x<y
+>= Katta yoki Teng x>=y
+<= Kichik yoki ting x<=y
+"""
+# mantiqiy operatorlar
+"""
+and- agar ikki shart ham rost bo'lsa , True qaytaradi
+or- kamida bitta shart rost bo'lsa True qaytaradi
+not- Shar qiymatini teskariga o'girish yani True bo'lsa False Yoke False bo'lsa True
+"""
+# Aniqlash operatorlar
+"""
+is- ikkila o'zgarovchi ham bir xil obekt bo'lsa True aks holda False
+is not- obektlar bir xil bo'lmasa True aks holda False
+"""
+# A'zolik operatorlar
+"""
+in- belgilangan qiymat obektda mavjud bo'lsa True aks holda False
+in not - belgilangan qiymat obektda mavjud bo'lmsa True aks holda False
+"""
+# Bitle Operatorlar
+"""
+&(and)
+|(or)
+~(not)
+"""
+
